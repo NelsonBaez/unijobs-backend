af9c4734e25b3787321cd79a90561b9411225612
MileneDanelli <mi.danelli@outlook.com>
2021-06-08 19:51:41 -0300

    Tratamento de Excessoes.

--- src/main/java/com/uniamerica/unijobsbackend/SwaggerConfig.java
+++ src/main/java/com/uniamerica/unijobsbackend/SwaggerConfig.java
@@ -37,7 +37,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
 
     private ApiInfo metaData() {
         return new ApiInfoBuilder()
-                .title("Projeto Unijobs - API Documentation")
+                .title("Projeto Produtos - API Documentation")
                 .description("Exemplo simples de documentação no Swagger")
                 .version("1.0.0")
                 .license("Apache License Version 2.0")
